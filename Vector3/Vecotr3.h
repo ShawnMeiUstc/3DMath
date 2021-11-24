c18a948824bc2b4e9a81fafe9e81ce344262d674
Shawn_Mei <mrmei1991@163.com>
2021-11-24 14:53:15 +0800

    Vector3.h

--- Vector3/Vecotr3.h
+++ Vector3/Vecotr3.h
@@ -105,3 +105,10 @@ inline Vector3 crossProduct(const Vector3& lhs, const Vector3& rhs) {
 	);
 }
 
+inline float distance(const Vector3& a, const Vector3& b) {
+	float dx = a.x - b.x;
+	float dy = a.y - b.y;
+	float dz = a.z - b.z;
+	return sqrt(dx * dx + dy * dy + dz * dz);
+}
+
